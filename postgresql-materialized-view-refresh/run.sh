3c40f350752c1f5b5313777a9f4e5cc481392ddd
Jamie Furtner <jamie@furtner.ca>
2022-08-30 16:07:50 -0600

    ShellCheck issues reviewed and fixed

--- postgresql-materialized-view-refresh/run.sh
+++ postgresql-materialized-view-refresh/run.sh
@@ -1,34 +1,34 @@
 #!/usr/bin/env bashio
 
 LOG_LEVEL=$(bashio::config 'log_level' 'info')
-bashio::log.level $LOG_LEVEL
+bashio::log.level "$LOG_LEVEL"
 
 bashio::log.debug "get database_connection_url"
 #postgresql://homeassistant:PASSWORD_GOES_here@77b2833f-timescaledb/homeassistant
 DBURL=$(bashio::config 'database_connection_url')
 
 bashio::log.debug "process db parameters"
-dbProto="$(echo $DBURL | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+dbProto="$(echo "$DBURL" | grep :// | sed -e's,^\(.*://\).*,\1,g')"
 # remove the protocol
-dbUrlCooked="$(echo ${DBURL/$dbProto/})"
+dbUrlCooked="${DBURL/$dbProto/}"
 # extract the user (if any)
-dbUserPass="$(echo $dbUrlCooked | grep @ | cut -d@ -f1)"
-dbPass="$(echo $dbUserPass | grep : | cut -d: -f2)"
+dbUserPass="$(echo "$dbUrlCooked" | grep @ | cut -d@ -f1)"
+dbPass="$(echo "$dbUserPass" | grep : | cut -d: -f2)"
 if [ -n "$dbPass" ]; then
-	dbUser="$(echo $dbUserPass | grep : | cut -d: -f1)"
+	dbUser="$(echo "$dbUserPass" | grep : | cut -d: -f1)"
 else
 	dbUser=$dbUserPass
 fi
 
 # extract the host
-dbHost="$(echo ${dbUrlCooked/$dbUserPass@/} | cut -d/ -f1)"
+dbHost="$(echo "${dbUrlCooked/$dbUserPass@/}" | cut -d/ -f1)"
 # fails due to set -o pipefail
 # by request - try to extract the port
 #set +o pipefail
 #dbPort="$(echo $dbHost | grep : | sed -e 's,.*:,:,g' -e 's,.*:\([0-9]*\),\1,g' -e 's,[^0-9],,g')"
 #set -o pipefail
 # extract the path (if any)
-dbName="$(echo $dbUrlCooked | grep / | cut -d/ -f2-)"
+dbName="$(echo "$dbUrlCooked" | grep / | cut -d/ -f2-)"
 
 unset dbUserPass
 unset dbUrlCooked
@@ -53,7 +53,7 @@ chmod 600 $PGPASSFILE
 echo "$dbHost:$dbPort:$dbName:$dbUser:$dbPass" > $PGPASSFILE
 
 bashio::log.info "Starting loop"
-while `true` ; do
+while true ; do
 	loop_datetime=$(date +%s)
 	bashio::log.debug "start loop $loop_datetime"
 	for view in $(bashio::config 'views|keys') ; do
@@ -72,16 +72,16 @@ while `true` ; do
 
 		last_changed=0
 		if [[ -f $statFile ]] ; then
-			last_changed=$(stat -c%Y $statFile)
+			last_changed=$(stat -c%Y "$statFile")
 		fi
 
-		let next_change_time="last_changed + (refresh_frequency_minutes * 60)"
+		next_change_time=$(( last_changed + (refresh_frequency_minutes * 60) ))
 		bashio::log.debug "last changed: $last_changed next change time: $next_change_time"
 
 		if [[ $next_change_time -lt $loop_datetime ]] ; then
 			bashio::log.debug "updating view $viewname"
-			psql --host $dbHost --username $dbUser --dbname $dbName --no-psqlrc --single-transaction  --command "SET TIME ZONE '$dbTimeZone'; REFRESH MATERIALIZED VIEW \"$viewname\"; " || true
-			touch $statFile
+			psql --host "$dbHost" --username "$dbUser" --dbname "$dbName" --no-psqlrc --single-transaction  --command "SET TIME ZONE '$dbTimeZone'; REFRESH MATERIALIZED VIEW \"$viewname\"; " || true
+			touch "$statFile"
 			bashio::log.debug "Update complete"
 		fi
 
